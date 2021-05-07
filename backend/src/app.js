188abb170eb3f7ee45af64b23d8bee3f8e227a62
Igor Rocha <cinatit.igor.santos@gmail.com>
2021-05-07 14:59:03 -0300

    Chore: install nodemon

---
+++ backend/src/app.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!')
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Server is up and running at http://localhost:${PORT}`)
+})

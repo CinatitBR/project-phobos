09e3be0f93a1abdf0e13b17f2fc8efb8f6bf17dc
Igor Rocha <cinatit.igor.santos@gmail.com>
2021-05-12 17:30:59 -0300

    Refactor: include routes in separate file

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,60 +1,14 @@
-import path from 'path'
 import express from 'express'
-import multer from 'multer'
+import router from './routes.js'
 import cors from 'cors'
-import { v4 as uuidv4 } from 'uuid'
-import PdfModel from './Models/PdfModel.js'
 
 const app = express()
-const mimeExtensions = { 'application/pdf': 'pdf' }
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const { userId } = req.body
-    const { mimetype } = file
-
-    const fileExtension = mimeExtensions[mimetype]
-
-    const storagePath = 'C:\\Users\\Igor\\Documents\\phobos-storage'
-    const userFolder = `user${userId}`
-    const fileStoragePath = path.join(storagePath, userFolder, fileExtension)
-
-    cb(null, fileStoragePath)
-  },
-
-  filename: async (req, file, cb) => {
-    const { userId } = req.body
-    const { mimetype, originalname } = file
-
-    const fileExtension = mimeExtensions[mimetype] 
-    const filename = `${uuidv4()}.${fileExtension}`
-    const title = path.parse(originalname).name
-
-    try {
-      await PdfModel.create({filename, title,  userId})
-
-      cb(null, filename)
-    }
-    catch (e) {
-      cb(e)
-    }
-  }
-})
-
-const upload = multer({ storage })
+const PORT = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
+app.use(router)
 
-app.get('/', (req, res) => {
-  res.send('Hello')
-})
-
-app.post('/pdf/upload', upload.single('pdf'), (req, res) => {
-  res.json({message: 'ok'})
-})
-
-const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Server is up and running at http://localhost:${PORT}`)
 })

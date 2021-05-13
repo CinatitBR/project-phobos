201d779f68e6aad70ec644906b98b95058c771f0
Igor Rocha <cinatit.igor.santos@gmail.com>
2021-05-12 21:42:24 -0300

    Refactor: define methods outside storage object

--- backend/src/middlewares/handleUpload.js
+++ backend/src/middlewares/handleUpload.js
@@ -5,41 +5,43 @@ import PdfModel from '../models/PdfModel.js'
 
 const mimeExtensions = { 'application/pdf': 'pdf' }
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const { userId } = req.body
-    const { mimetype } = file
+const setDestination = (req, file, cb) => {
+  const { userId } = req.body
+  const { mimetype } = file
 
-    const fileExtension = mimeExtensions[mimetype]
+  const fileExtension = mimeExtensions[mimetype]
 
-    const storagePath = 'C:\\Users\\Igor\\Documents\\phobos-storage'
-    const userFolder = `user${userId}`
-    const fileStoragePath = path.join(storagePath, userFolder, fileExtension)
+  const storagePath = 'C:\\Users\\Igor\\Documents\\phobos-storage'
+  const userFolder = `user${userId}`
+  const fileStoragePath = path.join(storagePath, userFolder, fileExtension)
 
-    cb(null, fileStoragePath)
-  },
+  cb(null, fileStoragePath)
+}
 
-  filename: async (req, file, cb) => {
-    const { userId } = req.body
-    const { mimetype, originalname } = file
+const setFilename = async (req, file, cb) => {
+  const { userId } = req.body
+  const { mimetype, originalname } = file
 
-    const fileExtension = mimeExtensions[mimetype] 
-    const filename = `${uuidv4()}.${fileExtension}`
-    const title = path.parse(originalname).name
+  const fileExtension = mimeExtensions[mimetype] 
+  const filename = `${uuidv4()}.${fileExtension}`
+  const title = path.parse(originalname).name
 
-    try {
-      await PdfModel.create({filename, title,  userId})
+  try {
+    await PdfModel.create({filename, title,  userId})
 
-      cb(null, filename)
-    }
-    catch (e) {
-      cb(e)
-    }
+    cb(null, filename)
   }
+  catch (e) {
+    cb(e)
+  }
+}
+
+const storage = multer.diskStorage({
+  destination: setDestination,
+  filename: setFilename
 })
 
 const upload = multer({ storage })
-
 const handleUpload = {
   pdf: upload.single('pdf')
 }

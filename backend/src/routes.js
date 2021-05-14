ea7cc73d7ea71f629d7f717eda305f9a3aa11cc6
Igor <cinatit.igor.santos@gmail.com>
2021-05-13 22:28:44 -0300

    Feat: extract data from uploaded pdf
    Use pdfParser to extract pdf data to pdfData object

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,13 +1,34 @@
+import path from 'path'
 import express from 'express'
 import handleUpload from './middlewares/handleUpload.js'
+import PDFParser from 'pdf2json'
 
 const router = express.Router()
+const pdfParser = new PDFParser(null, 1);
 
 router.get('/', (req, res) => {
   res.send('Hello')
 })
 
 router.post('/pdf/upload', handleUpload('pdf'), (req, res) => {
+  const { userId } = req.body
+  const { filename } = req.file
+
+  const storagePath = process.env.STORAGE_PATH
+  const userFolder = `user${userId}`
+
+  const filePath = path.join(storagePath, userFolder, 'pdf', filename)
+  
+  pdfParser.on("pdfParser_dataError", errData => { 
+    console.error(errData.parserError)
+  })
+
+  pdfParser.on("pdfParser_dataReady", pdfData => {
+    console.log(pdfData)
+  });
+
+  pdfParser.loadPDF(filePath)
+
   res.json({message: 'ok'})
 })
 

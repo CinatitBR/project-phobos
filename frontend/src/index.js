b16b24232d70c69043a923d26bb09552e5b115ca
Igor Rocha <cinatit.igor.santos@gmail.com>
2021-05-23 16:24:49 -0300

    Feat: install React

---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RegisterView from './views/RegisterView'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <RegisterView />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
---
+++ frontend/src/views/RegisterView.js
@@ -0,0 +1,11 @@
+import './index.css'
+
+const RegisterView = () => {
+  return (
+    <div>
+      <h1>Olá, tudo bem com você?</h1>
+    </div>
+  )
+}
+
+export default RegisterView

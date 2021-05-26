b3146d8d879d4010e77057fecfeaaa266fde98e4
Igor Rocha <cinatit.igor.santos@gmail.com>
2021-05-26 01:35:59 -0300

    Feat: create global css file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import RegisterView from './views/RegisterView'
 
+import './global.css'
+
 ReactDOM.render(
   <React.StrictMode>
     <RegisterView />

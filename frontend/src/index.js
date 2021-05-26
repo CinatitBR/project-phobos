17c6a0493bf2ce60bc765cdba2564c2969a955e0
Igor Rocha <cinatit.igor.santos@gmail.com>
2021-05-26 16:11:41 -0300

    Feat: use React Router to display Register page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Register from './pages/Register'
 
 import './global.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Register />
+    <Router>
+
+      <Route path="/register">
+        <Register />
+      </Route>
+
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );

c6481c388df6aaa88599a9f65f4c1b9ccbbb3ace
Igor Rocha <cinatit.igor.santos@gmail.com>
2021-05-26 02:06:25 -0300

    Feat: create SplitPaneRight

---
+++ frontend/src/components/SplitPaneRight/index.js
@@ -0,0 +1,11 @@
+import './index.css'
+
+const SplitPaneRight = ({ children }) => {
+  return (
+    <section id="splitPaneRight">
+      {children}
+    </section>
+  )
+}
+
+export default SplitPaneRight

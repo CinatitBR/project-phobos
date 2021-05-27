05ea7b928f573f07395bd90aded518cb8e3e931b
Igor Rocha <cinatit.igor.santos@gmail.com>
2021-05-27 19:12:13 -0300

    Feat: include validationMiddelware on route

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,9 @@
 import express from 'express'
 import handleUpload from './middlewares/handleUpload.js'
 import pdfController from './controllers/pdfController.js'
+import userController from './controllers/userController.js'
+import validationMiddleware from './middlewares/validationMiddleware.js'
+import registerSchema from '../../shared/src/validations/registerSchema.js'
 
 const router = express.Router()
 
@@ -9,5 +12,7 @@ router.get('/', (req, res) => {
 })
 
 router.post('/pdf/upload', handleUpload('pdf'), pdfController.upload)
+
+router.post('/user/register', validationMiddleware(registerSchema), userController.register)
 
 export default router

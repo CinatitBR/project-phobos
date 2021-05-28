bc26a8704f5a4f755faa15ce936d4987e6e6f541
Igor Rocha <cinatit.igor.santos@gmail.com>
2021-05-28 19:17:09 -0300

    Feat: add more props

--- frontend/src/components/FormField/index.js
+++ frontend/src/components/FormField/index.js
@@ -1,12 +1,28 @@
 import './index.css'
 
-const FormField = ({ label, type }) => {
+const FormField = ({ 
+  label, 
+  type, 
+  value, 
+  onChange, 
+  onBlur,
+  name, 
+  touched, 
+  error 
+}) => {
   return (
     <div id="formField">
       <label>
         {label}
 
-        <input type={type} />
+        <input 
+          type={type} 
+          value={value} 
+          name={name} 
+          onChange={onChange}
+          onBlur={onBlur} 
+        />
+        <span id="error">{touched && error}</span>
       </label>
     </div>
   )

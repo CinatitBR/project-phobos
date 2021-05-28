dfebaf186058e0ef38b899e99a760d49540f12b3
Igor Rocha <cinatit.igor.santos@gmail.com>
2021-05-28 16:46:39 -0300

    Refactor: use createValidaton on schema
    createValidation exposes validation methods

---
+++ shared/src/validations/registerValidation.js
@@ -0,0 +1,27 @@
+import * as yup from 'yup'
+import axios from 'axios'
+import createValidation from '../utils/createValidation.js'
+
+const emailExists = async (email) => {
+  const response = await axios.post('http://localhost:5000/user/find-by-email', {
+    email: email
+  })
+
+  const user = response.data
+
+  return user.length > 0
+}
+
+const registerSchema = yup.object({
+  email: yup.string().email().required().test(
+    'email-exists',
+    'This email already exists',
+    async email => !(await emailExists(email))
+  ),
+  username: yup.string().trim().max(255).required(),
+  password: yup.string().trim().max(255).required()
+})
+
+const registerValidation = createValidation(registerSchema)
+
+export default registerValidation

9bd986d9a096a8777c7de0a1d44dd8c525dcbdc7
Igor Rocha <cinatit.igor.santos@gmail.com>
2021-05-27 21:44:58 -0300

    Fix: trim white spaces

--- shared/src/validations/registerSchema.js
+++ shared/src/validations/registerSchema.js
@@ -2,8 +2,8 @@ import * as yup from 'yup'
 
 const registerSchema = yup.object({
   email: yup.string().email().required(),
-  username: yup.string().max(255).required(),
-  password: yup.string().max(255).required()
+  username: yup.string().trim().max(255).required(),
+  password: yup.string().trim().max(255).required()
 })
 
 export default registerSchema

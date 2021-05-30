9cdc20af08cd7852a5f9bf7a32158ffada9b887e
Igor Rocha <cinatit.igor.santos@gmail.com>
2021-05-30 17:22:13 -0300

    Refactor: change env variable name
    Change from "PORT" to "SERVER_PORT"

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,7 @@ import router from './routes.js'
 import cors from 'cors'
 
 const app = express()
-const PORT = process.env.PORT
+const PORT = process.env.SERVER_PORT
 
 app.use(cors())
 app.use(express.json())

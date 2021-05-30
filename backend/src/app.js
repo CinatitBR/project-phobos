e015fb4883e6085b47eb2f5eb37e1bbb3707d5a2
Igor Rocha <cinatit.igor.santos@gmail.com>
2021-05-30 20:54:14 -0300

    Feat: add cookieParser middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import router from './routes.js'
 import cors from 'cors'
+import cookieParser from 'cookie-parser'
 
 const app = express()
 const PORT = process.env.SERVER_PORT
 
 app.use(cors())
+app.use(cookieParser)
 app.use(express.json())
 app.use(router)
 

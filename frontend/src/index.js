49ed12a99bd41ae745917118218d9c0a5871cfe7
Igor Rocha <cinatit.igor.santos@gmail.com>
2021-06-04 02:45:26 -0300

    Feat: add Switch component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import Register from './pages/Register'
 import Login from './pages/Login'
@@ -9,15 +9,17 @@ import './global.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Route path="/register">
-        <Register />
-      </Route>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/register">
+          <Register />
+        </Route>
 
-      <Route path="/login">
-        <Login />
-      </Route>
-    </Router>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );

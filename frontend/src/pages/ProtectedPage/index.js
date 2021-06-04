9cfc7954066bd61d7a3460ad3d82e345d8ad0ee0
Igor Rocha <cinatit.igor.santos@gmail.com>
2021-06-04 16:04:00 -0300

    Feat: create ProtectedPage component
    It will be used for testing the authentication system

---
+++ frontend/src/pages/ProtectedPage/index.js
@@ -0,0 +1,8 @@
+const ProtectedPage = () => (
+  <>
+    <h1>Hello, this is a protected page</h1>
+    <h2>You should only see this if you are logged in</h2>
+  </>
+)
+
+export default ProtectedPage

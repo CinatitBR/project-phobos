daa2c97aab906037a73a53000e88a46bf258b3b4
Igor Rocha <cinatit.igor.santos@gmail.com>
2021-06-04 18:49:23 -0300

    Feat: create useAuth hook

---
+++ frontend/src/hooks/useAuth.js
@@ -0,0 +1,25 @@
+import { useContext, createContext } from "react"
+import { useProvideAuth } from './useProvideAuth'
+
+const authContext = createContext()
+
+// Hook for child components to get the auth object ...
+// ... and re-render when it changes.
+const useAuth = () => {
+  return useContext(authContext);
+}
+
+// Provider component that wraps your app and makes auth object ...
+// ... available to any child component that calls useAuth().
+const ProvideAuth = ({ children }) => {
+  const auth = useProvideAuth()
+
+  return (
+    <authContext.Provider value={auth}>
+      {children}
+    </authContext.Provider>
+  )
+}
+
+export { ProvideAuth }
+export default useAuth

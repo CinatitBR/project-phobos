52c26e503a47b5b16b0d97991d09aa016172b755
Igor Rocha <cinatit.igor.santos@gmail.com>
2021-10-01 12:16:59 -0300

    refactor: refactor function

---
+++ backend/src/utils/getPages.js
@@ -0,0 +1,28 @@
+// Receive pdf2json json file and get PDF pages
+const getPages = pdfData => {
+  const pages = []
+  
+  const { Pages } = pdfData.formImage
+
+  for (const [index, page] of Pages.entries()) {
+    const { Texts } = page
+    const pageData = {number: index + 1, body: ''}
+
+    for (const textBlock of Texts) {
+      const { R } = textBlock
+  
+      for (const textRun of R) {
+        const { T } = textRun
+  
+        pageData.body += `${T} `
+      }
+    }
+
+    pageData.body = decodeURIComponent(pageData.body)
+    pages.push(pageData)
+  }
+
+  return pages
+}
+
+export default getPages

60701d281c5c5831e82cc2c489c450992561b374
Igor Rocha <cinatit.igor.santos@gmail.com>
2021-10-04 14:25:09 -0300

    refactor: enable multipleStatements

--- backend/src/db.js
+++ backend/src/db.js
@@ -4,7 +4,8 @@ const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
+  database: process.env.DB_DATABASE,
+  multipleStatements: true,
 })
 
 export default db

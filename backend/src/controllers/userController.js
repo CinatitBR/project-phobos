99067b200841bf99d6e8a4b3afa8ed43b591c9f0
Igor Rocha <cinatit.igor.santos@gmail.com>
2021-10-08 15:01:28 -0300

    testing

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,19 +1,20 @@
 import userModel from '../models/userModel.js'
 
 const findByEmail = async (req, res) => {
-  try {
-    const { email } = req.body
-    const user = await userModel.findByEmail(email)
+  res.json({ message: 'Olá meu amigo' })
+  // try {
+  //   const { email } = req.body
+  //   const user = await userModel.findByEmail(email)
 
-    res.json(user)
-  }
-  catch(e) {
-    console.log(e)
+  //   res.json(user)
+  // }
+  // catch(e) {
+  //   console.log(e)
     
-    return res
-      .status(500)
-      .json({message: 'Oh, no! There was an error. Please, try again'})
-  }
+  //   return res
+  //     .status(500)
+  //     .json({message: 'Oh, no! There was an error. Please, try again'})
+  // }
 }
 
 const userController = { findByEmail }

cdec865f8690f77c13ea9eb17fec761c2e2309aa
Igor Rocha <cinatit.igor.santos@gmail.com>
2021-10-13 03:28:51 -0300

    refactor: refactor Portal component

--- frontend/src/components/Portal/index.js
+++ frontend/src/components/Portal/index.js
@@ -1,17 +1,9 @@
-import { useEffect } from "react"
 import { createPortal } from 'react-dom'
 
 const mountPoint = document.querySelector('#react-portal')
-const container = document.createElement('div')
 
 const Portal = ({ children }) => {
-  useEffect(() => {
-    mountPoint.appendChild(container)
-
-    return () => mountPoint.removeChild(container)
-  }, [])
-
-  return createPortal(children, container)
+  return createPortal(children, mountPoint)
 }
 
 export default Portal

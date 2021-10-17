508dc5eb2694ba9ff3da697bb8fd707d9393d1be
Igor Rocha <cinatit.igor.santos@gmail.com>
2021-10-17 01:15:10 -0300

    update style

--- frontend/src/components/Overlay/index.js
+++ frontend/src/components/Overlay/index.js
@@ -1,17 +1,7 @@
-import { useEffect } from 'react'
 import Portal from '../Portal'
 import style from './style.module.css'
 
 const Overlay = ({ children, showBackgroundColor = true, onClose }) => {
-
-  useEffect(() => {
-    // Remove page scroll
-    document.body.classList.add('noScroll')
-
-    // Add page scroll
-    return () => document.body.classList.remove('noScroll')
-  })
-
   return (
     <Portal>
       <div 

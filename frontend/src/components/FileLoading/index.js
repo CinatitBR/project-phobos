33ab504c0ed01f8a2d17dacf313a76164e21574f
Igor Rocha <cinatit.igor.santos@gmail.com>
2021-10-17 23:43:15 -0300

    feat: Create Collapse

--- frontend/src/components/FileLoading/index.js
+++ frontend/src/components/FileLoading/index.js
@@ -1,30 +1,97 @@
-import { FaFilePdf, FaTimes } from 'react-icons/fa'
+import { useState, useRef, useEffect } from 'react'
+import { FaFilePdf, FaTimes, FaChevronUp } from 'react-icons/fa'
+import FormField from '../FormField'
+import Select from '../Select'
 
 import style from './style.module.css'
 
 const FileLoading = ({ id, filename, size, uploaded, progress, onFileDelete }) => {
+  const [isCollapseOpen, setIsCollapseOpen] = useState(false)
+  const collapseEle = useRef(null)
+
+  const selectItems = [
+    {
+      text: 'Olá',
+    },
+    {
+      text: 'Mano tudo bem com você',
+    },
+    {
+      text: 'Com certeza deus'
+    }
+  ]
+
+  // Handle Collapse opening and closing
+  useEffect(() => {
+    // Check if ref is null
+    if (!collapseEle) return null
+
+    const { scrollHeight } = collapseEle.current
+
+    if (isCollapseOpen) {
+      collapseEle.current.style.maxHeight = `${scrollHeight + 30}px`
+      collapseEle.current.style.padding = '15px'
+    } 
+    else {
+      collapseEle.current.style.maxHeight = '0'
+      collapseEle.current.style.padding = '0'
+    }
+
+  }, [isCollapseOpen])
+  
   return (
     <article className={style.fileLoading}>
-      <FaFilePdf className={style.fileIcon} />
+      <div className={style.header}>
+        <span 
+          className={`${style.iconWrapper} ${style.collapseIcon}`}
+          onClick={() => setIsCollapseOpen(!isCollapseOpen)}
+        >
+          <FaChevronUp className={`${style.collapseIcon} ${style.icon}`} />
+        </span>
 
-      <div className={style.wrapper}>
-        <div className={style.info}>
-          <h3 className={style.filename}>{filename}</h3>
-          
-          <span className={style.fileSize}>{size} mb</span>
-
-          {uploaded &&
+        {uploaded &&
+          <span className={`${style.iconWrapper} ${style.close}`}>
             <FaTimes 
-              className={style.closeIcon} 
+              className={`${style.closeIcon} ${style.icon}`} 
               onClick={() => onFileDelete(id)} 
             />
-          }
-        </div>
+          </span>
+        }
+      </div>
 
-        <div className={style.progressBar}>
-          <div className={style.progressBarInner} style={{ width: `${progress}%` }}></div>
+      <div className={style.body}>
+        <FaFilePdf className={style.fileIcon} />
+
+        <div className={style.content}>
+          <div className={style.info}>
+            <h3 className={style.filename}>{filename}</h3>
+            
+            <span className={style.fileSize}>{size} mb</span>
+          </div>
+
+          <div className={style.progressBar}>
+            <div className={style.progressBarInner} style={{ width: `${progress}%` }}></div>
+          </div>
         </div>
       </div>
+      
+      <section 
+        className={style.collapseWrapper}
+        ref={collapseEle}
+      >
+        <div className={style.content}>
+          <FormField 
+            label="Add new tag"
+          />
+
+          <span className={style.text}>Or</span>
+          
+          <Select
+            label="Select existing tag"
+            items={selectItems}
+          />
+        </div>
+      </section>
     </article>
   )
 }

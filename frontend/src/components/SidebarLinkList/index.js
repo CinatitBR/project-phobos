413d413737a69b06ee62405ae646ef9912671c3f
Igor Rocha <cinatit.igor.santos@gmail.com>
2021-11-23 13:55:39 -0300

    refactor: remove SidebarLink folder

--- frontend/src/components/SidebarLinkList/index.js
+++ frontend/src/components/SidebarLinkList/index.js
@@ -1,6 +1,19 @@
-import SidebarLink from '../SidebarLink'
+import { NavLink } from 'react-router-dom'
 
 import './index.css'
+
+function SidebarLink({ icon, name, to, ...rest }) {
+  return (
+    <NavLink 
+      className="sidebarLink" 
+      exact
+      to={to}
+      activeClassName="active"
+      {...rest}>
+      {icon} {name}
+    </NavLink>
+  )
+}
 
 function SidebarLinkList({ sidebarLinks, ...rest }) {
   return (

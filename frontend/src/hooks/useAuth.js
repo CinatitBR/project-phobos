83b2dd481a865fa9f824ce397dfe93145d635413
Igor Rocha <cinatit.igor.santos@gmail.com>
2021-11-24 20:35:28 -0300

    refactor: include useProvideAuth inside useAuth

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -1,7 +1,82 @@
-import { useContext, createContext } from 'react'
-import useProvideAuth from './useProvideAuth'
+import { useState, useEffect, useContext, createContext } from 'react'
+import authAPI from '../apis/authAPI'
 
 const authContext = createContext()
+
+// Provider hook that creates auth object and handles state
+const useProvideAuth = () => {
+  const [user, setUser] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  const register = async ({ email, username, password }) => {
+    try {
+      await authAPI.register({ email, username, password })
+    }
+    catch (error) {
+      const errors = error.response.data
+      
+      throw errors
+    }
+  }
+
+  const login = async ({ email, password }) => {
+    try {
+      const response = await authAPI.login({ email, password })
+      const { accessToken, user } = response.data
+
+      authAPI.setAuthHeader(`Bearer ${accessToken}`)
+      setUser(user)
+    }
+    catch (error) {
+      const errors = error.response.data
+
+      throw errors
+    }
+  }
+
+  const logout = async () => {
+    try {
+      await authAPI.logout()
+
+      setUser(null)
+    }
+    catch (error) {
+      const errors = error.response.data
+
+      throw errors
+    }
+  }
+
+  const refreshToken = async () => {
+    try {
+      const response = await authAPI.refreshToken()
+      const { accessToken, user } = response.data
+  
+      setUser(user)
+      authAPI.setAuthHeader(`Bearer ${accessToken}`)
+    }
+    catch (error) {
+      setUser(null)
+    }
+    finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    refreshToken()
+  }, [])
+
+  const auth = { 
+    user, 
+    loading, 
+    register, 
+    login, 
+    logout 
+  }
+
+  return auth
+}
 
 // Hook for child components to get the auth object ...
 // ... and re-render when it changes.

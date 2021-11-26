c6759603aae7d0d71420b56f37432116f2490855
Igor Rocha <cinatit.igor.santos@gmail.com>
2021-11-26 18:32:24 -0300

    refactor: refactor component structure

--- frontend/src/components/FilePreviewSidebar/index.js
+++ frontend/src/components/FilePreviewSidebar/index.js
@@ -1,10 +1,53 @@
 import FileTag from '../FileTag'
-import { FaFileAlt, FaFilePdf, FaArrowDown, FaTrashAlt } from 'react-icons/fa'
+import { FaFileAlt, FaFilePdf, FaArrowDown, FaTrashAlt, FaExternalLinkAlt } from 'react-icons/fa'
 
 import style from './style.module.css'
 
-const FilePreviewSidebar = () => {
+const FileInfo = ({ title, size, tag }) => (
+  <div className={style.fileInfo}>
+    <FaFilePdf className={style.fileIcon} />
+    <h2 className={style.filename}>{title}</h2>
 
+    <div className={style.footer}>
+      <div className={style.fileSize}>
+        <header>Size</header>
+        <span className={style.size}>{size} MB</span>
+      </div>
+
+      <FileTag>{tag}</FileTag>
+    </div>
+  </div>
+)
+
+const ButtonList = () => (
+  <div className={style.buttonList}>
+    <div className={style.item}>
+      <span className={style.backgroundIcon}>
+        <FaExternalLinkAlt className={style.icon} />
+      </span>
+
+      <span className={style.buttonName}>Open</span>
+    </div>
+
+    <div className={style.item}>
+      <span className={style.backgroundIcon}>
+        <FaTrashAlt className={style.icon} />
+      </span>
+
+      <span className={style.buttonName}>Delete</span>
+    </div>
+
+    <div className={style.item}>
+      <span className={style.backgroundIcon}>
+        <FaArrowDown className={style.icon} />
+      </span>
+
+      <span className={style.buttonName}>Download</span>
+    </div>
+  </div>
+)
+
+const FilePreviewSidebar = () => {
   return (
     <div className={style.filePreviewSidebar}>
       <div className={style.title}>
@@ -13,50 +56,15 @@ const FilePreviewSidebar = () => {
 
       <div className={style.divider}></div>
 
-      <div className={style.fileInfo}>
-        <FaFilePdf className={style.fileIcon} />
-        <h2 className={style.filename}>Probabilidade.pdf</h2>
-
-        <div className={style.footer}>
-          <div className={style.fileSize}>
-            <header>Size</header>
-            <span className={style.size}>10 MB</span>
-          </div>
-
-          <FileTag>Matemática</FileTag>
-        </div>
-      </div>
+      <FileInfo 
+        title="Probabilidade.pdf"
+        tag="Matemática"
+        size="10"
+      />
 
       <div className={style.divider} style={{ margin: ' 0-20px' }}></div>
 
-      <div className={style.buttonList}>
-
-        <div className={style.button}>
-          <span className={style.backgroundIcon}>
-            <FaArrowDown className={style.icon} />
-          </span>
-
-          <span className={style.buttonName}>Open</span>
-        </div>
-
-        <div className={style.button}>
-          <span className={style.backgroundIcon}>
-            <FaTrashAlt className={style.icon} />
-          </span>
-
-          <span className={style.buttonName}>Delete</span>
-        </div>
-
-        <div className={style.button}>
-          <span className={style.backgroundIcon}>
-            <FaArrowDown className={style.icon} />
-          </span>
-
-          <span className={style.buttonName}>Download</span>
-        </div>
-
-      </div>
-
+      <ButtonList />
     </div>
   )
 }

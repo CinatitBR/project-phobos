1a6dd3b502fd4f5bcf18998981d92d38c893547b
Igor Rocha <cinatit.igor.santos@gmail.com>
2022-01-15 02:10:15 -0300

    Migrate refreshToken methods to PostgreSQL api

--- backend/src/models/refreshTokenModel.js
+++ backend/src/models/refreshTokenModel.js
@@ -1,41 +1,85 @@
-import db from '../db.js'
+import db, { pool } from '../db.js'
+
+// const create = async (userId, refreshToken) => {
+//   try {
+//     const query = `
+//       INSERT INTO refresh_token (user_id, refresh_token) 
+//       VALUES (?, ?)    
+//     `
+
+//     await db.query(query, [userId, refreshToken])
+//   }
+//   catch (e) {
+//     throw new Error(e)
+//   }
+// }
 
 const create = async (userId, refreshToken) => {
   try {
     const query = `
       INSERT INTO refresh_token (user_id, refresh_token) 
-      VALUES (?, ?)    
+      VALUES ($1, $2)    
     `
 
-    await db.query(query, [userId, refreshToken])
+    await pool.query(query, [userId, refreshToken])
   }
   catch (e) {
     throw new Error(e)
   }
 }
+
+// const destroy = async (refreshToken) => {
+//   try {
+//     const query = `
+//       DELETE FROM refresh_token 
+//       WHERE refresh_token = ?
+//     `
+
+//     await db.query(query, refreshToken)
+//   }
+//   catch (e) {
+//     throw new Error(e)
+//   }
+// }
 
 const destroy = async (refreshToken) => {
   try {
     const query = `
       DELETE FROM refresh_token 
-      WHERE refresh_token = ?
+      WHERE refresh_token = $1
     `
 
-    await db.query(query, refreshToken)
+    await pool.query(query, [refreshToken])
   }
   catch (e) {
     throw new Error(e)
   }
 }
 
+// const find = async (refreshToken) => {
+//   try {
+//     const query = `
+//       SELECT * FROM refresh_token
+//       WHERE refresh_token = ?
+//     `
+
+//     const [rows, fields] = await db.query(query, refreshToken)
+
+//     return rows[0]
+//   }
+//   catch (e) {
+//     throw new Error(e)
+//   }
+// }
+
 const find = async (refreshToken) => {
   try {
     const query = `
       SELECT * FROM refresh_token
-      WHERE refresh_token = ?
+      WHERE refresh_token = $1
     `
 
-    const [rows, fields] = await db.query(query, refreshToken)
+    const { rows } = await pool.query(query, [refreshToken])
 
     return rows[0]
   }

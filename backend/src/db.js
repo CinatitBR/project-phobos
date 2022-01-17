9f8ad104cd1dc3f1cd3308719df33f4a3b4f16c4
Igor Rocha <cinatit.igor.santos@gmail.com>
2022-01-16 22:46:53 -0300

    Refactor: create DATABASE_URI env

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,13 +1,10 @@
 import pkg from 'pg';
 
 const { Pool } = pkg;
+const connectionString = process.env.DATABASE_URI;
 
 export const pool = new Pool({
-  host: process.env.PG_HOST,
-  user: process.env.PG_USER,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
+  connectionString
 })
 
 export default pool;

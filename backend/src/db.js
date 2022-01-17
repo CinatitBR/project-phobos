d35e82d412703b7d53956eb5abba43eb9705d1ba
Igor Rocha <cinatit.igor.santos@gmail.com>
2022-01-16 23:14:01 -0300

    Fix: update env name

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,7 +1,7 @@
 import pkg from 'pg';
 
 const { Pool } = pkg;
-const connectionString = process.env.DATABASE_URI;
+const connectionString = process.env.DATABASE_URL;
 
 export const pool = new Pool({
   connectionString
